24ba21e62373526e420c8c442c67dd77a4c201e4
mich balkany <askmitchb@gmail.com>
2023-12-25 09:39:28 -0500

    Initial Commit

---
+++ FriendFace-60/FriendFace_60App.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFace_60App.swift
+//  FriendFace-60
+//
+//  Created by Mich balkany on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFace_60App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
